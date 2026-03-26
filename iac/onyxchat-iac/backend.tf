929470a7088613645ca2562cb0a4250d74c972d0
Cole Schmidt <colecschmidt@gmail.com>
2026-03-26 17:05:25 -0500

    fix: update S3 bucket name for production environment in backend configuration

--- iac/onyxchat-iac/backend.tf
+++ iac/onyxchat-iac/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "onyxchat-tfstate-cole-2025"
+    bucket         = "onyxchat-tfstate-prod-001"
     key            = "onyxchat/prod/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "onyxchat-tflock"
