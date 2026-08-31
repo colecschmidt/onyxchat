96cb6c80dd4bf881a0b219c23792c166a46ddcf3
Cole Schmidt <colecschmidt@gmail.com>
2026-08-30 20:55:57 -0500

    debug: log readiness ping error

--- server/onyxchat-server/internal/http/health_handlers.go
+++ server/onyxchat-server/internal/http/health_handlers.go
@@ -2,6 +2,7 @@ package http
 
 import (
 	"context"
+	"log"
 	"net/http"
 	"time"
 )
@@ -24,10 +25,11 @@ func ReadyHandler(userStore userStorer) http.HandlerFunc {
 		defer cancel()
 
 		if err := userStore.Ping(ctx); err != nil {
-			w.Header().Set("Content-Type", "application/json")
-			w.WriteHeader(http.StatusServiceUnavailable)
-			_, _ = w.Write([]byte(`{"status":"not_ready"}`))
-			return
+				log.Printf("readiness ping failed: %v", err)
+				w.Header().Set("Content-Type", "application/json")
+				w.WriteHeader(http.StatusServiceUnavailable)
+				_, _ = w.Write([]byte(`{"status":"not_ready"}`))
+				return
 		}
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
